296dc0048c9c68a2ca8ec9dae1d7a520522217bd
saidmurod <saidmurod217@gmail.com>
2025-11-23 17:58:06 +0500

    entityManager

---
+++ src/main/java/org/example/config/EntityManagerConfig.java
@@ -0,0 +1,35 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class EntityManagerConfig {
+
+    private final DataSource dataSource;
+
+    public EntityManagerConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+        entityManager.setDataSource(dataSource);
+        entityManager.setPackagesToScan("org.example.model");
+        entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties props = new Properties();
+        props.put("hibernate.hbm2ddl.auto", "update");
+        props.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        props.put("hibernate.show_sql", "true");
+        entityManager.setJpaProperties(props);
+
+        return entityManager;
+    }
+}
---
+++ src/main/java/org/example/config/TransactionConfig.java
@@ -0,0 +1,16 @@
+package org.example.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+
+
+@Configuration
+public class TransactionConfig {
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
