d2348aaea29f7a86df4b97379f12182eab3343bf
saidmurodxamidov-cloud <saidmurod_xamidov@epam.com>
2025-11-24 19:20:17 +0500

    Entity/trainee (#19)
    * datasource config with docker container of pgadmin
    
    * entityManager
    
    * fixed:
    
    * fix
    
    * entity manager and transaction configs
    
    * main changed
    
    * removed uneccary things
    
    * fixed issues
    
    * added user entity
    
    * toString
    
    * naming strategy and little bug fix
    
    * snake case
    
    * entity manager fix
    
    * added char[] to string and vice versa converter
    
    * added trainee entity
    
    * user_id fix
    
    ---------
    
    Co-authored-by: saidmurod <saidmurod217@gmail.com>
    Co-authored-by: saidmurod <s.xamidov@newuu.uz>

---
+++ src/main/java/org/example/entity/TraineeEntity.java
@@ -0,0 +1,30 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Past;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = {"id"})
+public class TraineeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @Past
+    private LocalDate dateOfBirth;
+
+    private String address;
+
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+}
