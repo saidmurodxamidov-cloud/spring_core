30337a086a3bffd175125d487abbc58b70304667
saidmurod <saidmurod217@gmail.com>
2025-11-24 12:09:15 +0500

    removed unwanted entity

--- src/main/java/org/example/config/DataSourceConfig.java
+++ src/main/java/org/example/config/DataSourceConfig.java
@@ -1,7 +1,6 @@
 package org.example.config;
 
 import com.zaxxer.hikari.HikariDataSource;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
