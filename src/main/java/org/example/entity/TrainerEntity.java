8d796bafd1b8eec7c3c88ef3c60df5034a91cf58
saidmurod <saidmurod217@gmail.com>
2025-11-28 18:31:49 +0500

    trainer entity implemented with relations

---
+++ src/main/java/org/example/entity/TrainerEntity.java
@@ -0,0 +1,46 @@
+package org.example.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = {"id"})
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "trainer")
+public class TrainerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    @ToString.Exclude
+    private UserEntity user;
+
+    @ManyToMany
+    @JoinTable(
+            name = "trainer_specializations",
+            joinColumns = @JoinColumn(name = "trainer_id"),
+            inverseJoinColumns = @JoinColumn(name = "training_type_id")
+    )
+    private Set<TrainingTypeEntity> specializations = new HashSet<>();
+
+
+    @ManyToMany
+    @JoinTable(
+            name = "trainers_trainee",
+            joinColumns = @JoinColumn(name = "trainer_id"),
+            inverseJoinColumns = @JoinColumn(name = "trainee_id")
+    )
+    @ToString.Exclude
+    private Set<TraineeEntity> trainees = new HashSet<>();
+}
--- src/main/java/org/example/entity/TrainingTypeEntity.java
+++ src/main/java/org/example/entity/TrainingTypeEntity.java
@@ -3,10 +3,16 @@ package org.example.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "training_type")
+@ToString
 public class TrainingTypeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -14,4 +20,7 @@ public class TrainingTypeEntity {
 
     private String trainingTypeName;
 
+    @ManyToMany(mappedBy = "specializations")
+    @ToString.Exclude
+    private Set<TrainerEntity> trainerEntities = new HashSet<>();
 }
