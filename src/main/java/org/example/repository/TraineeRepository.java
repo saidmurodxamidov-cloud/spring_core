61d1010e22d145ba9ec1045810609f287b5b216b
saidmurod <saidmurod217@gmail.com>
2025-12-01 00:10:04 +0500

    Added Trainee and Trainer repo

---
+++ src/main/java/org/example/repository/TraineeRepository.java
@@ -0,0 +1,21 @@
+package org.example.repository;
+
+import org.example.entity.TraineeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TraineeRepository extends JpaRepository<TraineeEntity,Long> {
+    Optional<TraineeEntity> findByUserUserName(String username);
+
+    Optional<TraineeEntity> findByUserUserNameAndUserPassword(String username,char[] password);
+
+    void deleteByUserUserName(String username);
+
+    List<TraineeEntity> findByUserIsActiveTrue();
+}
