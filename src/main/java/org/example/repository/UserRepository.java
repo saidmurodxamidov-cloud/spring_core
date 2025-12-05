dab06c48f06950d4809ebdccdfb70d9bacd2cfe0
saidmurod <s.xamidov@newuu.uz>
2025-12-05 21:54:55 +0500

    fix

--- src/main/java/org/example/repository/UserRepository.java
+++ src/main/java/org/example/repository/UserRepository.java
@@ -2,17 +2,13 @@ package org.example.repository;
 
 import org.example.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.Set;
-=======
-import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
->>>>>>> origin/main
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
@@ -21,10 +17,7 @@ public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
     boolean existsByUserName(String username);
 
-<<<<<<< HEAD
     @Query("SELECT u.userName FROM UserEntity u")
     Set<String> findAllUserNames();
 
-=======
->>>>>>> origin/main
 }
