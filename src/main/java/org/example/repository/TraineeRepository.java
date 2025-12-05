f705b5d7a1f3807f2e2c3bfaf978b928f9c7d9bf
saidmurodxamidov-cloud <saidmurod_xamidov@epam.com>
2025-12-05 07:28:31 +0500

    Repo/trainer trainee (#23)
    * added repos
    
    * Added Trainee and Trainer repo
    
    * changed repo method and name of entity field
    
    * fixed
    
    * removed unused methods
    
    ---------
    
    Co-authored-by: saidmurod <saidmurod217@gmail.com>

---
+++ src/main/java/org/example/repository/TraineeRepository.java
@@ -0,0 +1,19 @@
+package org.example.repository;
+
+import org.example.entity.TraineeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TraineeRepository extends JpaRepository<TraineeEntity,Long> {
+    Optional<TraineeEntity> findByUserUserName(String username);
+
+    void deleteByUserUserName(String username);
+
+    List<TraineeEntity> findByUserIsActive(boolean isActive);
+}
---
+++ src/main/java/org/example/repository/TrainerRepository.java
@@ -0,0 +1,21 @@
+package org.example.repository;
+
+import org.example.entity.TrainerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TrainerRepository extends JpaRepository<TrainerEntity,Long> {
+    Optional<TrainerEntity> findByUserUserName(String username);
+
+    @Query("SELECT t FROM TrainerEntity t WHERE t NOT IN " +
+            "(SELECT tr FROM TraineeEntity tn JOIN tn.trainers tr WHERE tn.user.userName = :traineeUsername)")
+    List<TrainerEntity> findTrainersNotAssignedToTrainee(@Param("traineeUsername") String traineeUsername);
+
+    List<TrainerEntity> findByUserIsActive(boolean isActive);
+}
