4ab2558c7894b2195299ba13465db383876603b0
saidmurod <saidmurod217@gmail.com>
2025-12-10 20:03:37 +0500

    removed unneccassary method

--- src/main/java/org/example/service/TrainerEntityService.java
+++ src/main/java/org/example/service/TrainerEntityService.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
 
@@ -30,6 +31,7 @@ public class TrainerEntityService {
     private final PasswordEncoder passwordEncoder;
     private final TrainerMapper trainerMapper;
 
+    @Transactional
     public TrainerDTO createTrainer(TrainerDTO trainerDTO){
         Set<String> availableUsernames = userRepository.findAllUserNames();
         String password = bcrypt.encode(String.valueOf(trainerDTO.getPassword()));
